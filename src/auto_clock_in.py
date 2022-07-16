0cd7885b7e2fc4a9e730046003a0234d941069fe
IceyBlackTea <IceyBlackTea@outlook.com>
2022-07-16 11:34:03 +0800

    ✨ limit the max num of repeat times

--- src/auto_clock_in.py
+++ src/auto_clock_in.py
@@ -2,8 +2,8 @@
 Author: IceyBlackTea
 Date: 2021-11-05 10:30:32
 LastEditors: IceyBlackTea
-LastEditTime: 2022-07-02 11:06:30
-FilePath: /clock-in-everyday/auto_clock_in.py
+LastEditTime: 2022-07-16 11:31:32
+FilePath: /clock-in-everyday/src/auto_clock_in.py
 Description: Copyright © 2021 IceyBlackTea. All rights reserved.
 '''
 
@@ -14,6 +14,7 @@ import utils
 def clock_in(config):
     try:
         flag = False
+        repeat_times = 0
 
         while True:
             hour = utils.get_local_hour()
@@ -24,7 +25,7 @@ def clock_in(config):
 
                 if config['send_mail']:
                     utils.send_mail(config['mail_config'], 'Oh No', content)
-                
+
             if flag and hour != 1:
                 utils.wait_until_next_hour()
                 continue
@@ -32,10 +33,18 @@ def clock_in(config):
             flag = False
 
             try:
-                token, headers = utils.sign_in(config['baidu_ocr'], config['sign_in_config'])
+                token, headers = utils.sign_in(
+                    config['baidu_ocr'], config['sign_in_config'])
 
             except Exception as e:
                 utils.handle_error(e)
+                repeat_times += 1
+
+                if repeat_times > 2:
+                    repeat_times = 0
+                    utils.print_logs(
+                        "always sign in failed, reach max repeat times!")
+                    utils.wait_until_next_hour()
 
             else:
                 try:
@@ -44,15 +53,17 @@ def clock_in(config):
 
                 except Exception as e:
                     utils.handle_error(e)
-                    
+
                 else:
                     flag = True
-                    
+                    repeat_times = 0
+
                     content = 'Time to sleep!'
 
                     if config['send_mail']:
-                        utils.send_mail(config['mail_config'], 'Clock In', content)
-                        
+                        utils.send_mail(
+                            config['mail_config'], 'Clock In', content)
+
                     utils.wait_until_next_hour()
 
     except KeyboardInterrupt:
@@ -65,5 +76,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
